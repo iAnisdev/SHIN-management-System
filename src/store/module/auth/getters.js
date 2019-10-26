2a642d5e3364ed73e1c934bad1a707705b80e768
Anis <marahman049@gmail.com>
2019-10-26 19:47:27 +0800

    Build Update

--- src/store/module/auth/getters.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    getLoginStatus: (state) => {
-        return state.isLoggedIN
-    },
-    getUserToken: (state) => {
-        return state.userToken
-    }
-}
--- src/store/module/auth/mutations.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    SET_LOGIN_STATUS: (state, status) => {
-        state.isLoggedIN = status
-    },
-    SET_USER_TOKEN: (state, tok) => {
-        state.userToken = tok
-    }
-}
